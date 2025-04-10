7a00929c089b1b4055d378be17b233a2e9f12f60
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-04-10 14:43:14 +0100

    each email grid

--- website/js/.js
+++ website/js/.js
@@ -6,16 +6,16 @@ const submit = document.getElementById('submit');
 const form = document.getElementsByTagName('form')[0];
 
 const email_regex = new RegExp(String.raw`^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$`);
+const url = "https://picsum.photos/256?random="
 
 randomindex = 0;
 function newCurrentImage() {
     // get size dynamically?
     randomindex += 1;
-    currentImage.src = `https://picsum.photos/256?random=${randomindex}`;
-    currentImage.i = randomindex;
+    currentImage.src = url + randomindex;
 }
 
-function invalidEmail() {
+function validEmail() {
     if (email_regex.test(email.value)) {
         email.className = 'valid'
         email.setCustomValidity("");
@@ -23,7 +23,7 @@ function invalidEmail() {
     } else {
         email.className = 'invalid'
         if (email.value == '') {
-            email.setCustomValidity("Email address is required");
+            email.setCustomValidity("Please enter an email address");
         } else {
             email.setCustomValidity("Invalid email address");
         }
@@ -32,12 +32,12 @@ function invalidEmail() {
 }
 
 document.getElementById('submit').addEventListener('click', () => {
-    if (invalidEmail()) {
+    if (validEmail()) {
         // display error message
     }
 });
 document.getElementById('email').addEventListener('focusout', () => {
-    invalidEmail();
+    validEmail();
 });
 form.addEventListener('submit', (event) =>  {
     event.preventDefault();
@@ -45,8 +45,43 @@ form.addEventListener('submit', (event) =>  {
 });
 
 
+function getAssignElement(email) {
+    for (box of assignedImages.children) {
+        const emails = box.getElementsByClassName('email');
+        if (emails.length == 0) {continue;}
+        if (emails[0].textContent == email) {
+            assignedImages.prepend(box);
+            return box;
+        }
+    }
+    return null;
+}
+
 function assignCurrentImage(email) {
+    let emailAssign = getAssignElement(email);
+    if (emailAssign == null) {
+        emailAssign = document.createElement('div');
+        emailAssign.classList.add('box');
+        assignedImages.prepend(emailAssign);
+    }
+    let grids = emailAssign.getElementsByClassName('img-grid');
+    if (grids.length == 0) {
+        emailAssign.innerHTML = `<h2 class="email">${email}</h2>
+            <div class="img-grid">
+            </div>`;
+        grids = emailAssign.getElementsByClassName('img-grid');
+    }
+    const grid = grids[0];
+    grid.innerHTML += `<img src=${url}${randomindex} alt />`;
+    newCurrentImage();
 }
 
 
 newCurrentImage();
+
+assignCurrentImage('test1');
+assignCurrentImage('test1');
+assignCurrentImage('test1');
+assignCurrentImage('test1');
+assignCurrentImage('test2');
+assignCurrentImage('test2');
